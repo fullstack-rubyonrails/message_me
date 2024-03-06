1ea3ec4be63b6c2b3333ba31313e90951d5550e2
Hassaan Ahmed Siddiqui <hassaan956@gmail.com>
2024-03-06 20:00:04 +0500

    Add routes for session create & destroy, add messages partial, login % logout links in navigation partial, javascript messages display, before action in session & chatroom controller

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,9 +13,6 @@ require("channels")
 require("custom/home")
 // End added by Hassaan
 
-// $(document).on('turbolinks:load', function() {
-//   $('.ui.dropdown').dropdown();
-// })
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
